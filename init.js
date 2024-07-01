a73d999d536ddb9550bdb8976cf7222448825891
GREED <159240548+YareJay@users.noreply.github.com>
2024-07-02 01:44:11 +0800

    Add files via upload

---
+++ init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown();
+    $('.modal').modal();
+    $('.fixed-action-btn').floatingActionButton({});
+    $('.slider').slider({
+      inDuration:1000,
+      outDuration:1000});
+    $('.materialboxed').materialbox();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
